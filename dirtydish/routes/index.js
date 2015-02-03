672585b3e4ec01da997c4d5fe099013c48c445f1
EC2 Default User <ec2-user@ip-172-31-36-150.us-west-2.compute.internal>
2015-02-03 20:32:22 +0000

    database started

--- dirtydish/routes/index.js
+++ dirtydish/routes/index.js
@@ -7,7 +7,131 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'DirtyDish', description: 'DirtyDish is on its way!!!' });
 });
 
+function restaurant() {
+	this.id: '',
+	this.name: '',
+	this.locality : '',
+	this.street_address: '', 
+	this.cuisines : '',
+	this.region : '',
+	this.lng: '',
+	this.phone : '',
+	this.postal_code : '',
+	this.has_menu: '',
+	this.country : '',
+	this.lat: '',
+	this.locu_id: '',
+	this.website_url : '',
+	this.twitter_id: '',
+	this.similar_ven : '',
+	this.open_hours: '',
+	this.added datetime: '',
+	this.menu_json text: '',
+	this.last_modified datetime: ''
+};
+
+function restaurantService(req) {
+
+	this.restaurantExistsInDB = function(locu_id) {
+
+		req.getConnection(function(err,conn){
+			if (err) return next("Cannot Connect");
+			var query = conn.query("SELECT id FROM restaurant where locu_id = '?'", locu_id, function(err,rows){
+			if(err){
+				console.log(err);
+				return next("Mysql error, check your query");
+			}
+			return rows[0].id!=undefined?true:false;
+        	});
+
+	};
+};
+
+function open_hours() {
+	id: '',
+	restaurant_id: '',
+	week_day: '',
+	open: '',
+	close: ''
+};
+
+function menu() {
+	id:'',
+        restaurant_id: '',
+	menu_name:'',
+	currency_symbol:''
+};
+
+function menu_section() {
+	menu_id:'',
+	section_name:''
+}
+
+function menu_section_subsection() {
+	id:'',
+	section_id:'',
+	subsection_name:'',
+	section_text1:'',
+        section_text2:'',
+        section_text3:'',
+        section_text4:'',
+        section_text5:''
+};
+
+function menu_section_subsection_item() {
+	id:'',
+	subsection_id:'',
+	name:'',
+	description:'',
+	price:'',
+	photos:''
+};
+
+function optiongroup() {
+	id:'',
+	item_id:'',
+	type:'',
+	text:'',
+	description:''
+};
+
+function optiongroup_option() {
+	id:'',
+	optiongroup:'',
+	name:'',
+	price:''
+};
+
+function externalids() {
+	id:'',
+	restaurant_id:'',
+	external_id:'',
+	url:'',
+	mobile_url:''
+};
+
+function delivery() {
+	id: '',
+	restaurant_id:'',
+	will_deliver:'',
+	hours:'',
+	minimum_order:'',
+	areas:''
+};
+
+function media() {
+	id:'',
+	restaurant_id:'',
+	cover_photo:'',
+	venue_photos:'',
+	menu_photos:'',
+	logos:'',
+	videos:''
+};
+
+
 router.get('/restaurant/:v', function(req, res, next) {
+
 /*
 req.getConnection(function(err,conn){
 
